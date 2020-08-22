bb1b9b8547bd14e40806537ad0c60419b605ece5
kurt.iamele <kurt.iamele@gmail.com>
2020-08-22 13:30:29 -0500

    working db

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     "development": {
         "username": "root",
         "password": "password",
-        "database": "kurtdb",
+        "database": "userdb",
         "host": "localhost",
         "port": "3306",
         "dialect": "mysql"
