275544296d3dd771db5b919f62d77ff4e2eacbfa
kurt.iamele <kurt.iamele@gmail.com>
2020-08-28 18:43:07 -0500

    register redirect

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 
 class Register extends Component {
 
   render() {
     return (
-      <Router>
-
-        <div className="container">
 
 
-          <div className="col-md-6 mt-5 mx-auto">
-            <form action="/tutor" class="inline">
-              <button type="submit" className="btn btn-lg btn-primary btn-block" > I want to teach  </button>
-            </form>
-          </div>
+      <div className="container">
 
 
-          <div className="col-md-6 mt-5 mx-auto">
-            <form action="/student" class="inline">
-              <button type="submit" className="btn btn-lg btn-primary btn-block" > I want to learn  </button>
-            </form>
-          </div>
-
-
+        <div className="col-md-6 mt-5 mx-auto">
+          <form action="/tutor" class="inline">
+            <button type="submit" className="btn btn-lg btn-primary btn-block" > I want to teach  </button>
+          </form>
         </div>
 
-      </Router >
+
+        <div className="col-md-6 mt-5 mx-auto">
+          <form action="/student" class="inline">
+            <button type="submit" className="btn btn-lg btn-primary btn-block" > I want to learn  </button>
+          </form>
+        </div>
+
+
+      </div>
+
+
     )
   }
 
