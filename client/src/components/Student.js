e9fff9a262b045f9b9316978ee06bb10f64ff8c6
kurt.iamele <kurt.iamele@gmail.com>
2020-08-29 00:51:47 -0500

    fixed heroku routing

--- client/src/components/Student.js
+++ client/src/components/Student.js
@@ -58,90 +58,91 @@ class Student extends Component {
 
     render() {
         return (
+            <div className="container">
+                <div className="main">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-6 mt-5 mx-auto">
+                                <h1> Enter Student Info </h1>
+                                <form noValidate onSubmit={this.onSubmit}>
 
-            <div className="main">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6 mt-5 mx-auto">
-                            <h1> Enter Student Info </h1>
-                            <form noValidate onSubmit={this.onSubmit}>
+                                    <div className="form-group">
+                                        <label htmlFor="name">First name:</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="first_name"
+                                            placeholder="Enter your first name"
+                                            value={this.state.first_name}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="name">Last name:</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="last_name"
+                                            placeholder="Enter your last name"
+                                            value={this.state.last_name}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="email">Email address:</label>
+                                        <input
+                                            type="email"
+                                            className="form-control"
+                                            name="email"
+                                            placeholder="Enter email"
+                                            value={this.state.email}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="password">Password:</label>
+                                        <input
+                                            type="password"
+                                            className="form-control"
+                                            name="password"
+                                            placeholder="Password"
+                                            value={this.state.password}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="password">Grade:</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="grade"
+                                            placeholder="middel school"
+                                            value={this.state.grade}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
 
-                                <div className="form-group">
-                                    <label htmlFor="name">First name:</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        name="first_name"
-                                        placeholder="Enter your first name"
-                                        value={this.state.first_name}
-                                        onChange={this.onChange}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="name">Last name:</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        name="last_name"
-                                        placeholder="Enter your last name"
-                                        value={this.state.last_name}
-                                        onChange={this.onChange}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="email">Email address:</label>
-                                    <input
-                                        type="email"
-                                        className="form-control"
-                                        name="email"
-                                        placeholder="Enter email"
-                                        value={this.state.email}
-                                        onChange={this.onChange}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="password">Password:</label>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        name="password"
-                                        placeholder="Password"
-                                        value={this.state.password}
-                                        onChange={this.onChange}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="password">Grade:</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        name="grade"
-                                        placeholder="middel school"
-                                        value={this.state.grade}
-                                        onChange={this.onChange}
-                                    />
-                                </div>
+                                    <div className="form-group">
+                                        <label htmlFor="password">Student Subject:</label>
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            name="subject"
+                                            placeholder="Math"
+                                            value={this.state.subject}
+                                            onChange={this.onChange}
+                                        />
+                                    </div>
 
-                                <div className="form-group">
-                                    <label htmlFor="password">Student Subject:</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        name="subject"
-                                        placeholder="Math"
-                                        value={this.state.subject}
-                                        onChange={this.onChange}
-                                    />
-                                </div>
-
-                                <button type="submit" className="btn btn-lg btn-primary btn-block"
-                                    disabled={!this.state.valid_email || !this.state.password || !this.state.subject || !this.state.first_name} >
-                                    Register
+                                    <button type="submit" className="btn btn-lg btn-primary btn-block"
+                                        disabled={!this.state.valid_email || !this.state.password || !this.state.subject || !this.state.first_name} >
+                                        Register
                                 </button>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </div >
             </div >
         )
     }
