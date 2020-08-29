5d1896356d19bbd3da26bdbace98745e4a3e6a8d
kurt.iamele <kurt.iamele@gmail.com>
2020-08-29 00:17:09 -0500

    fixed heroku routing

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ app.use(
   })
 )
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  // app.use(express.static("client/build"));
+  app.use(express.static(__dirname + '/client/build'));
 }
 
 app.use(routes);
@@ -24,16 +25,16 @@ app.use(routes);
 
 db.sequelize
   .sync()
-  .then(function() {
+  .then(function () {
     // ...then start web server
-    app.listen(PORT, function(err) {
+    app.listen(PORT, function (err) {
       if (err) {
         return console.log(err);
       }
       console.log(`Server now running on http://localhost:${PORT}!`);
     });
   })
-  .catch(function(err) {
+  .catch(function (err) {
     console.log(err, "Something went wrong with the db sync!");
   });
 
