f5d2453b8e0ced420b3ae74a6eb99c6e3f2aa446
kurt.iamele <kurt.iamele@gmail.com>
2020-08-29 11:04:56 -0500

    fixed heroku routing

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,36 +1,44 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import { Button } from 'react-bootstrap';
+import Student from './Student';
+import Tutor from './Tutor';
 
 class Register extends Component {
 
   render() {
     return (
 
-      <div className="container">
 
-        <div className="col-md-6 mt-5 mx-auto">
 
-          {/* <button onclick="location.href='/tutor'" className="btn btn-lg btn-primary btn-block" > I want to teach  </button> */}
+      <Router>
+        <div className="App">
 
-          {/* <button onclick="window.location.href='/tutor'">Continue</button> */}
 
-          <form method="get" action="/tutor">
-            <button type="submit" className="btn btn-lg btn-primary btn-block">I want to teach </button>
-          </form>
+
+          <div className="container">
+
+            <div className="col-md-6 mt-5 mx-auto">
+              <form action="/tutor" component={Tutor} className="inline">
+                <button type="submit" className="btn btn-lg btn-primary btn-block" > I want to learn  </button>
+              </form>
+            </div>
+
+            <div className="col-md-6 mt-5 mx-auto">
+              <form action="/student" component={Student} className="inline">
+                <button type="submit" className="btn btn-lg btn-primary btn-block" > I want to learn  </button>
+              </form>
+            </div>
+
+
+          </div>
+
 
 
         </div>
 
-
-        <div className="col-md-6 mt-5 mx-auto">
-          <form action="/student" className="inline">
-            <button type="submit" className="btn btn-lg btn-primary btn-block" > I want to learn  </button>
-          </form>
-        </div>
-
-
-      </div>
-
+      </Router >
     )
   }
 
